7657317d0b5eb9a61c2de85b1a2925e89845d1b2
King_Search <greenkelechi@gmail.com>
2020-07-05 22:57:19 +0100

    Initial commit

---
+++ myapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Search(models.Model):
+    text = models.CharField(max_length=500)
+    added_date = models.DateTimeField
+
+    def __str__(self):
+        return '{}'.format(self.text)
+
+    class Meta:
+        verbose_name_plural = 'Search'
+
+
+# Create your models here.
